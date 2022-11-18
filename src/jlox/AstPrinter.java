0b7ed5efafcd249f56ec2b16955110ff3bc76730
neilshah2000 <neilshahlimited@hotmail.com>
2022-11-18 15:47:20 +0000

    inheritance

--- src/jlox/AstPrinter.java
+++ src/jlox/AstPrinter.java
@@ -47,6 +47,11 @@ public class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public String visitSuperExpr(Expr.Super expr) {
+        return null;
+    }
+
+    @Override
     public String visitThisExpr(Expr.This expr) {
         return null;
     }
